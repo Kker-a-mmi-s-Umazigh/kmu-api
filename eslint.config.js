ae0861d53fd4bca435852c7bd910db3c78fdea28
Ali <benmerad.ali@gmail.com>
2026-01-02 14:08:08 +0100

    Add ESLint v9 config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        Buffer: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+      },
+    },
+  },
+]
